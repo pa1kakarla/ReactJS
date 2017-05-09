d60a36759ff664ab774ad2b6d50cccf1f0394530
Pawan <pa1kakarla@gmail.com>
2017-05-09 11:20:55 -0400

    second commit

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import uuid from 'uuid';
 
 class AddProject extends Component {
     constructor(){
@@ -16,6 +16,7 @@ class AddProject extends Component {
             alert('Title is required');
         }else{
             this.setState({newProject:{
+                id:uuid.v4(),
                 title: this.refs.title.value,
                 category: this.refs.category.value
             }}, function(){
@@ -27,7 +28,7 @@ class AddProject extends Component {
     }
   render() {
       let categoryOptions = this.props.categories.map(category => {
-          return <option key={category} value="category">{category}</option>
+          return <option key={category} value={category}>{category}</option>
       });
     return (
       <div>
@@ -43,6 +44,7 @@ class AddProject extends Component {
                     {categoryOptions}
                     </select>
             </div>
+            <br />
             <input type="submit" value="Submit" />
         </form>
 
@@ -51,4 +53,8 @@ class AddProject extends Component {
   }
 }
 
+AddProject.propTypes = {
+  categories: React.PropTypes.array,
+  addProject: React.PropTypes.func
+}
 export default AddProject;
